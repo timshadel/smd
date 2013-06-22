50abd6c0c7263180c4d766701e76f30e5ed61c68
Tim Shadel <tim@shadelsoftware.com>
2013-06-21 18:54:27 -0600

    More tests

--- index.js
+++ index.js
@@ -14,20 +14,26 @@ var debug = require('simple-debug')
 var MS_PER_SECOND = 1000
   , MS_PER_MINUTE = 60 * MS_PER_SECOND
   , MS_PER_HOUR = 60 * MS_PER_MINUTE
-  , DEFAULT_RANGE = Math.pow(2, 16);
+  , INT_RANGE = Math.pow(2, 16);
 
 
 /**
  * Expose smd function as the module.
  */
 
-exports = module.exports = new SmD(MS_PER_HOUR, DEFAULT_RANGE);
+exports = module.exports = new SmD(MS_PER_HOUR, INT_RANGE);
 
-exports.minute = new SmD(MS_PER_MINUTE, DEFAULT_RANGE);
+exports.minute = new SmD(MS_PER_MINUTE, INT_RANGE);
 
-exports.custom = function(ms_per_unit, range) {
-  return new SmD(ms_per_unit || MS_PER_HOUR, range || DEFAULT_RANGE);
+exports.custom = function(config) {
+  config = config || {};
+  return new SmD(config.ms_per_unit || MS_PER_HOUR, config.range || INT_RANGE);
 }
+
+exports.MS_PER_SECOND = MS_PER_SECOND;
+exports.MS_PER_MINUTE = MS_PER_MINUTE;
+exports.MS_PER_HOUR = MS_PER_HOUR;
+exports.INT_RANGE = INT_RANGE;
 
 
 /**
@@ -63,11 +69,35 @@ SmD.prototype.at = function(date_ms) {
 };
 
 /**
+ * Earliest date we can represent
+ */
+
+SmD.prototype.min = function() {
+  return this.date(-1 * this.range/2);
+}
+
+/**
+ * Latest date we can represent
+ */
+
+SmD.prototype.max = function() {
+  return this.date(this.range-1);
+}
+
+/**
+ * Convert a super-small date back to a regular JavaScript Date object.
+ */
+
+SmD.prototype.time = function(date_units) {
+  return date_units * this.ms_per_unit + Math.floor(Date.now()/this.range_in_ms)*this.range_in_ms;
+}
+
+/**
  * Convert a super-small date back to a regular JavaScript Date object.
  */
 
 SmD.prototype.date = function(at) {
-  return new Date(at * this.ms_per_unit + Math.floor(Date.now()/this.range_in_ms)*this.range_in_ms);
+  return new Date(this.time(at));
 }
 
 /**
@@ -75,5 +105,5 @@ SmD.prototype.date = function(at) {
  */
 
 SmD.prototype.seconds_from_now = function(at) {
-  return Math.floor((this.when(at) - Date.now()) / MS_PER_SECOND);
+  return Math.floor((this.time(at) - Date.now()) / MS_PER_SECOND);
 }
--- test/index.test.js
+++ test/index.test.js
@@ -12,9 +12,68 @@ var SmD = require("../")
 
 describe("SmD", function(){
 
-  it("should return #now()", function() {
-    var now = SmD.now();
-    expect(now).to.be.a('number');
+  describe("#now", function(){
+    it("should be very close to Date.now()", function() {
+      var now = SmD.now();
+      var dtNow = Date.now();
+      var long = SmD.time(now);
+
+      expect(now).to.be.a('number');
+      expect(dtNow-long).to.be.lessThan(60 * 60 * 1000);
+    });
+  });
+
+  describe("#time", function(){
+    it("should support negative and positive numbers", function() {
+      var early = SmD.time(-1*Math.pow(2,15));
+      var late = SmD.time(Math.pow(2,16)-1);
+
+      // 2003-08-24T00:00:00Z
+      expect(early).to.be.equal(SmD.min().getTime());
+      // 2014-11-10T00:00:00Z
+      expect(late).to.be.equal(SmD.max().getTime());
+    });
+  });
+
+  describe("#at", function(){
+    it("should support negative and positive numbers", function() {
+      var smd = SmD.at(Date.now());
+      expect(smd).to.be.lessThan(SmD.range);
+    });
+  });
+
+  describe("#date", function(){
+    it("should return Date objects", function() {
+      var early = SmD.date(-1*Math.pow(2,15));
+      var late = SmD.date(Math.pow(2,16)-1);
+
+      // 2003-08-24T00:00:00Z
+      expect(early).to.eql(new Date(SmD.min()));
+      // 2014-11-10T00:00:00Z
+      expect(late).to.eql(new Date(SmD.max()));
+    });
+  });
+
+  describe("#min", function(){
+    it("should be -1/2 the range", function() {
+      var CustomSmD = SmD.custom({ range: 16 });
+      expect(CustomSmD.min()).to.eql(CustomSmD.date(-8));
+    });
+  });
+
+  describe("#max", function(){
+    it("should be the range - 1", function() {
+      var CustomSmD = SmD.custom({ range: 16 });
+      expect(CustomSmD.max()).to.eql(CustomSmD.date(15));
+    });
+  });
+
+  describe("#seconds_from_now", function(){
+    it("should be accurate, but rounded down", function() {
+      var CustomSmD = SmD.custom({ ms_per_unit: 1000 });
+      var seconds = CustomSmD.seconds_from_now(CustomSmD.now()+5);
+      expect(seconds).to.equal(4);
+    });
   });
 
 });
